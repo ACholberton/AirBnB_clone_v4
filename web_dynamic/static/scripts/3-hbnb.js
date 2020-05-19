5513d9a992f88b113672f1627fcf82256670a9e8
ACholberton <1103@holbertonschool.com>
2020-05-19 02:03:35 +0000

    added js script that appends deleted article section in html file

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -24,3 +24,21 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
     $('DIV#api_status').removeClass('available');
   }
 });
+
+$.ajax({
+    type:'POST',
+    url:'http://0.0.0.0:5001/api/v1/places_search/'
+    contentType:'application/json',
+    dataType:'json',
+    data:'{}',
+    success: function (data) {
+	for (const placeJson of Object.Values(data)) {
+	    $('section.places').append(
+		'<article>' + '<div class="title_box">' + '<h2>{{ place.name }}</h2>' + '<div class="price_by_night">${{ place.price_by_night }}</div>' + '</div>' + 
+		    '<div class="information">' + '<div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}' + '</div>' +
+		    '<div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}' + '</div>' + 
+		    '<div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}' + '</div>' +
+		    '</div>' + '<div class="description">' + '{{ place.description | safe }}' + '</div>' + '</article>');
+	}
+    }
+});
